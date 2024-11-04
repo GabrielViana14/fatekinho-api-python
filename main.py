406e1ec5ec8ef6381e0ef19e8c6b60183db386f5
GabrielViana14 <130351936+GabrielViana14@users.noreply.github.com>
2024-11-04 22:30:58 +0000

    alteração requirements.txt

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from rotas.clientes import router as clientes_router
 from rotas.usuarios import router as usuarios_router
 from rotas.autenticacao import router as auth_router
 
-
+# Rodar a API localmente: uvicorn main:app --reload
+# Rodar a API no github Codespace: uvicorn main:app --reload --host 0.0.0.0 --port 8000
 # Para testar API só usar a url: http://127.0.0.1:8000/docs
 # caso esteja em outra url é só adicionar o /docs no final da url
 
